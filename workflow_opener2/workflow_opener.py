9179531ee8c64c7f036e098eaf955b4a7659ccd0
YE <qyeaf@connect.ust.hk>
2022-05-11 00:01:57 +0800

    1. test minio to mq

--- workflow_opener2/workflow_opener.py
+++ workflow_opener2/workflow_opener.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import json
+
 import pika
 import requests
 
@@ -6,28 +8,29 @@ import requests
 MQ_KEY = 'hkust'
 MQ_SECRET = 'csit6000o'
 MQ_HOST = 'rabbitmq'
-MQ_PORT = '5672'
-QUEUE = 'markdown'
+MQ_PORT = 5672
+QUEUE = 'markdown2'
 
 # OpenFaaS configuration
-OF_HOST='faas'
-OF_PORT='60000'
+OF_HOST = 'faas'
+OF_PORT = '60000'
 URL = 'http://{}:{}/'.format(OF_HOST, OF_PORT)
 
 
-def callback(channel, method_frame, header_frame, body):
+def callback(c_channel, method_frame, header_frame, body):
     body = str(body, "utf-8")
-    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-    payload={'url': '{}'.format(body)}
+    data = json.loads(body)['Key'].replace('markdown/', '')
+    c_channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    payload = {'url': '{}'.format(data)}
 
-    requests.post(URL+'conversion', data=payload)
-    requests.post(URL+'sentiment', data=payload)
+    requests.post(URL + 'conversion', data=payload)
+    requests.post(URL + 'sentiment', data=payload)
 
 
-# Step #1: Connect to RabbitMQ using the default parameters
+# # Step #1: Connect to RabbitMQ using the default parameters
 credentials = pika.PlainCredentials(MQ_KEY, MQ_SECRET)
 parameters = pika.ConnectionParameters(host=MQ_HOST, port=MQ_PORT, virtual_host='/', credentials=credentials)
-connection = pika.BlockingConnection(parameters = parameters)
+connection = pika.BlockingConnection(parameters=parameters)
 channel = connection.channel()
 channel.queue_declare(queue=QUEUE, durable=True)
 channel.basic_consume(QUEUE, callback)
